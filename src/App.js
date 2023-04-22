e1bcab6a27ba1b4de484cf9906ee0ebe559b446c
MD Roknuzzaman <rokonmd111@gmail.com>
2023-04-22 17:55:49 +0600

    complete full project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Footer, Blog, Possibility, WhatGPT3, Header, Feature } from './container'
+import { CalltoAction, Brand, Navbar } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Feature></Feature>
+      <Possibility></Possibility>
+      <CalltoAction></CalltoAction>
+      <Blog></Blog>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from './article/Article'
+export { default as Brand } from './brand/Brand'
+export { default as CalltoAction } from './calltoAction/CalltoAction'
+export { default as Features } from './features/Features'
+export { default as Navbar } from './navbar/Navbar'
